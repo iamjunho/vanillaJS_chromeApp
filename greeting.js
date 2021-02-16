7295acde2daca098007a27635414f9a0af6bcd7c
Junho <iamjunho@gmail.com>
2021-02-17 00:53:47 +0900

    lecture #3.2 Saving the User Name part one

---
+++ greeting.js
@@ -0,0 +1,28 @@
+const form = document.querySelector(".js-form");
+const input = form.querySelector("input");
+const greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser";
+const SHOWING_ON = "showing";
+
+function paintGreeting(text){
+  form.classList.remove(SHOWING_ON);
+  greeting.classList.add(SHOWING_ON);
+  greeting.innerText = `Hello ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+
+  if(currentUser === null) {
+      // she is not
+  } else {
+    paintGreeting(currentUser);
+  }
+}
+
+function init() {
+  loadName();
+}
+
+init();
