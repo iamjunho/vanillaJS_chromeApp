20be8b7c2c600019302a8f17f4731b46e9ce7f3e
iamjunho@gmail.com <iamjunho@gmail.com>
2021-02-17 09:53:29 +0900

    lectore #3.3 Saving the User Name part two

--- greeting.js
+++ greeting.js
@@ -1,11 +1,27 @@
-const form = document.querySelector(".js-form");
-const input = form.querySelector("input");
-const greeting = document.querySelector(".js-greetings");
+const form = document.querySelector('.js-form');
+const input = form.querySelector('input');
+const greeting = document.querySelector('.js-greetings');
 
-const USER_LS = "currentUser";
-const SHOWING_ON = "showing";
+const USER_LS = 'currentUser';
+const SHOWING_ON = 'showing';
 
-function paintGreeting(text){
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_ON);
+  form.addEventListener('submit', handleSubmit);
+}
+
+function paintGreeting(text) {
   form.classList.remove(SHOWING_ON);
   greeting.classList.add(SHOWING_ON);
   greeting.innerText = `Hello ${text}`;
@@ -15,7 +31,7 @@ function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
 
   if(currentUser === null) {
-      // she is not
+      askForName();
   } else {
     paintGreeting(currentUser);
   }
